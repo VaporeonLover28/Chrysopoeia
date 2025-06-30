6f45af2d1d981f8265ba8c59b5c0ea528187aabb
Solon <walter.sobreira23@nave.org.br>
2025-06-29 22:00:28 -0300

    blackjack npc 1

--- blackjack_npc.gd
+++ blackjack_npc.gd
@@ -1,7 +1,11 @@
 extends CharacterBody3D
 
-func _ready() -> void:
-	pass # Replace with function body.
+@onready var nav: NavigationAgent3D = $nav
 
 func _process(delta: float) -> void:
-	pass
+	var destination = nav.get_next_path_position()
+	var local_destination = destination - global_position
+	var dir = local_destination.normalized()
+
+func _go_to_table(table, seat):
+	nav.set_target_position(table[seat])
--- blackjack_test_3.gd
+++ blackjack_test_3.gd
@@ -1,6 +1,15 @@
 extends Node3D
 
+@onready var npc = preload("res://bj_npc_test.tscn")
+
+var playing_npcs : Array = []
+
 func _call_player():
+	#instantiate new npc away from table (visual effect for testing)
+	#call the funcion on the npc to come to the table
+	#add the npc to the npc array
+	var new_npc = npc.instantiate()
+	new_npc.position = Vector3(randf_range(-3, 3), -0.342, -6)
 	print("Called Player")
 
 func _shuffle_deck():
