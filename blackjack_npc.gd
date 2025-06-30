8aff86bb5d6a47f5037706dfb1555af445266863
felipepreuss <waltercarvalho2904@gmail.com>
2025-06-30 09:41:28 -0300

    humping npc

--- blackjack_npc.gd
+++ blackjack_npc.gd
@@ -2,10 +2,16 @@ extends CharacterBody3D
 
 @onready var nav: NavigationAgent3D = $nav
 
+var assigned_chair : bool = false
+
 func _process(delta: float) -> void:
 	var destination = nav.get_next_path_position()
 	var local_destination = destination - global_position
 	var dir = local_destination.normalized()
+	velocity = dir * 2
+	
+	move_and_slide()
 
-func _go_to_table(table, seat):
-	nav.set_target_position(table[seat])
+func _go_to_table(chair_pos):
+	print(chair_pos)
+	nav.set_target_position(chair_pos)
--- blackjack_test_3.gd
+++ blackjack_test_3.gd
@@ -1,15 +1,36 @@
 extends Node3D
 
-@onready var npc = preload("res://bj_npc_test.tscn")
+@onready var npc = preload("res://blackjack_npc.tscn")
+@onready var table_node = $table
 
+var table : Array = []
 var playing_npcs : Array = []
+
+func _ready():
+	#adds a new chair in the table array
+	for chairs in table_node.get_child_count():
+		#excludes the table mesh from chair count
+		if table_node.get_child(chairs) is Marker3D:
+			#chair is [position, is_taken]
+			table.append([table_node.get_child(chairs).position, 0])
+			#print("New Chair, pos " + str(table[chairs - 1][0]))
+			#print(table)
 
 func _call_player():
 	#instantiate new npc away from table (visual effect for testing)
 	#call the funcion on the npc to come to the table
 	#add the npc to the npc array
 	var new_npc = npc.instantiate()
-	new_npc.position = Vector3(randf_range(-3, 3), -0.342, -6)
+	new_npc.position = Vector3(randf_range(-3, 3), 0.342, -6)
+	add_child(new_npc)
+	for chairs in table.size():
+		if table[chairs][1] == 0 and new_npc.assigned_chair == false:
+			print("Chair Position: " + str(table[chairs][0]))
+			new_npc._go_to_table(table[chairs][0])
+			table[chairs][1] = 1
+			new_npc.assigned_chair = true
+		else:
+			pass
 	print("Called Player")
 
 func _shuffle_deck():
