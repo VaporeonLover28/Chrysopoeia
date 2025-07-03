00531b85b03a0022f31d27891dd2f8f22542543f
VaporeonLover28 <waltercarvalho2904@gmail.com>
2025-07-03 14:13:27 -0300

    card layout
    - finish marker recognition
    - instantiate suit on markers

---
+++ card.gd
@@ -0,0 +1,12 @@
+extends Node3D
+
+var rank : String = "A"
+var suit : String = "Spades"
+
+@onready var suitmarkers: Node3D = $cardinfo/suitmarkers
+
+func _ready() -> void:
+	pass # Replace with function body.
+
+func _process(delta: float) -> void:
+	pass
