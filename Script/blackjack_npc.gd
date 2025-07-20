ba1e5d104916d36922c0bc71d8c64475f6ab271c
Solon <walter.sobreira23@nave.org.br>
2025-07-19 22:53:34 -0300

    blackjack npc AI (missing stuff)
    to-do:
    - dealer turn
    - visual feedback for player
    - remove test stuff
    - polish
    - implement into world

--- Script/blackjack_npc.gd
+++ Script/blackjack_npc.gd
@@ -3,9 +3,11 @@ extends CharacterBody3D
 @onready var nav: NavigationAgent3D = $nav
 @onready var hand_marker: Marker3D = $hand
 @onready var action_label: Label3D = $action_label
+@onready var game: Node3D = $".."
+@onready var player: CharacterBody3D = $"../bj_player_test"
 
-@export_enum("Pleb", "Mage", "Guard", "Noble", "Joker" ) var type : String = "Noble"
-@export_enum("Playing", "Standed", "Busted", "Surrendered", "Blackjack", "Doubled") var state : String = "Playing"
+@export_enum("Pleb", "Mage", "Guard", "Noble", "Joker" ) var personality : String = "Noble"
+@export_enum("Playing", "Standed", "Busted", "Blackjack", "Doubled") var state : String = "Playing"
 
 var assigned_chair : bool = false
 var is_sat_down : bool = false
@@ -13,25 +15,276 @@ var is_sat_down : bool = false
 var assigned_gc_label : RichTextLabel
 var hand : Array = []
 var hand_value : int = 0
+var aces_in_hand = 0
 
 func _process(delta: float) -> void:
 	if is_sat_down == false:
 		var destination = nav.get_next_path_position()
 		var local_destination = destination - global_position
 		var dir = local_destination.normalized()
-		velocity = dir * 2
+		velocity = dir * 4
 		look_at(transform.basis.z * -1)
 		rotation.x = 0
 		rotation.z = 0
 	elif velocity != Vector3.ZERO:
 		velocity = Vector3.ZERO
 		#_look_at_player()
-	
 	move_and_slide()
+
+func play_turn():
+	calculate_hand_value()
+	print(name + "'s turn ")
+	if state == "Playing":
+		match personality:
+			"Noble":
+				if aces_in_hand == 0:
+					match player.dealer_hand[1].value:
+						"2":
+							hard_total_matrix(
+							"stand",\
+							"stand",\
+							"stand",\
+							"stand",\
+							"stand",\
+							"hit",\
+							"double_down",\
+							"double_down",\
+							"hit",\
+							"hit")
+						"3":
+							hard_total_matrix(
+							"stand",\
+							"stand",\
+							"stand",\
+							"stand",\
+							"stand",\
+							"hit",\
+							"double_down",\
+							"double_down",\
+							"double_down",\
+							"hit")
+						"4", "5", "6":
+							hard_total_matrix(
+							"stand",\
+							"stand",\
+							"stand",\
+							"stand",\
+							"stand",\
+							"stand",\
+							"double_down",\
+							"double_down",\
+							"double_down",\
+							"hit")
+						"7", "8", "9":
+							hard_total_matrix(
+							"stand",\
+							"stand",\
+							"hit",\
+							"hit",\
+							"hit",\
+							"hit",\
+							"double_down",\
+							"double_down",\
+							"hit",\
+							"hit")
+						"10", "J", "Q", "K", "A":
+							hard_total_matrix(
+							"stand",\
+							"stand",\
+							"hit",\
+							"hit",\
+							"hit",\
+							"hit",\
+							"double_down",\
+							"hit",\
+							"hit",\
+							"hit")
+				else:
+					match player.dealer_hand[1].value:
+						"2":
+							soft_total_matrix(
+							"stand",\
+							"stand",\
+							"double_down",\
+							"hit",\
+							"hit",\
+							"hit",\
+							"hit")
+						"3":
+							soft_total_matrix(
+							"stand",\
+							"stand",\
+							"double_down",\
+							"double_down",\
+							"hit",\
+							"hit",\
+							"hit")
+						"4":
+							soft_total_matrix(
+							"stand",\
+							"stand",\
+							"double_down",\
+							"double_down",\
+							"double_down",\
+							"hit",\
+							"hit")
+						"5":
+							soft_total_matrix(
+							"stand",\
+							"stand",\
+							"double_down",\
+							"double_down",\
+							"double_down",\
+							"double_down",\
+							"hit")
+						"6":
+							soft_total_matrix(
+							"stand",\
+							"double_down",\
+							"double_down",\
+							"double_down",\
+							"double_down",\
+							"double_down",\
+							"double_down")
+						"7", "8":
+							soft_total_matrix(
+							"stand",\
+							"stand",\
+							"stand",\
+							"hit",\
+							"hit",\
+							"hit",\
+							"hit")
+						"9", "10", "J", "Q", "K", "A":
+							soft_total_matrix(
+							"stand",\
+							"stand",\
+							"hit",\
+							"hit",\
+							"hit",\
+							"hit",\
+							"hit")
+	else:
+		match state:
+			"Standed":
+				print(name + " has standed, skipping turn.")
+			"Busted":
+				print(name + " has busted, skipping turn.")
+			"Blackjack":
+				print(name + " has a blackjack, so they stand.")
+			"Doubled":
+				print(name + " has doubled, so they cannot hit.")
+
+func calculate_hand_value():
+	var value_bank = 0
+	aces_in_hand = 0
+	
+	for card in hand:
+		match card.value:
+			"A":
+				value_bank += 11
+				aces_in_hand += 1
+			"2", "3", "4", "5", "6", "7", "8", "9", "10":
+				value_bank += int(card.value)
+			"J", "Q", "K":
+				value_bank += 10
+	
+	if value_bank > 21:
+		if aces_in_hand > 0 and value_bank - 10 < 22:
+			value_bank -= 10
+		elif aces_in_hand > 1 and value_bank - 20 < 22 and value_bank - 10 > 22:
+			value_bank -= 20
+		elif aces_in_hand > 1 and value_bank - 20 < 22 and value_bank - 10 < 22:
+			value_bank -= 10
+		else:
+			bust()
+	
+	if value_bank == 21 and hand.size() == 2 and aces_in_hand == 1:
+		blackjack()
+	
+	hand_value = value_bank
+	update_gc_label()
+
+func hit():
+	update_action_label("hit")
+	print(name + " hits.")
+	game.card_to_npc(game.deck.pop_back(), self, Vector3.ZERO)
+	calculate_hand_value()
+
+func stand():
+	update_action_label("stand")
+	state = "Standed"
+	print(name + " stands.")
+
+func double_down():
+	update_action_label("double")
+	state = "Doubled"
+	print(name + " doubles down.")
+	game.card_to_npc(game.deck.pop_back(), self, Vector3(-180, 90, 0))
+	calculate_hand_value()
+
+func bust():
+	update_action_label("bust")
+	state = "Busted"
+	print(name + " busts.")
+
+func blackjack():
+	update_action_label("blackjack")
+	state = "Blackjack"
+	print(name + " has a blackjack.")
+
+func hard_total_matrix(case1, case2, case3, case4, case5, case6, case7, case8, case9, case10):
+	match hand_value:
+		21, 20, 19, 18:
+			case_match(case1)
+		17:
+			case_match(case2)
+		16:
+			case_match(case3)
+		15:
+			case_match(case4)
+		14, 13:
+			case_match(case5)
+		12:
+			case_match(case6)
+		11:
+			case_match(case7)
+		10:
+			case_match(case8)
+		9:
+			case_match(case9)
+		8, 7, 6, 5, 4:
+			case_match(case10)
+
+func soft_total_matrix(case1, case2, case3, case4, case5, case6, case7):
+	match hand_value:
+		20:
+			case_match(case1)
+		19:
+			case_match(case2)
+		18:
+			case_match(case3)
+		17:
+			case_match(case4)
+		16, 15:
+			case_match(case5)
+		14, 13:
+			case_match(case6)
+		12:
+			case_match(case7)
+
+func case_match(case):
+	match case:
+		"hit":
+			hit()
+		"stand":
+			stand()
+		"double_down":
+			double_down()
 
 func update_gc_label():
 	assigned_gc_label.text = "Player " + name + "\nAI: " + \
-	str(type) + "\nValue: " + str(hand_value) + "\nHand:\n"
+	str(personality) + "\nValue: " + str(hand_value) + "\nHand:\n"
 	for cards in hand:
 		assigned_gc_label.text += str(cards.rank) + " of " + str(cards.suit) + ",\n"
 
--- Script/blackjack_test_3.gd
+++ Script/blackjack_test_3.gd
@@ -7,6 +7,7 @@ extends Node3D
 @onready var gamecontrol: Panel = $CanvasLayer/gamecontrol
 @onready var player: CharacterBody3D = $bj_player_test
 @onready var card_distr_timer: Timer = $card_distr_timer
+@onready var turn_timer: Timer = $turn_timer
 
 var tween : Tween
 
@@ -17,6 +18,7 @@ var round : int = 0
 var playing_npcs : Array = []
 var gc_labels : Array = []
 var round_order_npcs : Array = []
+var whose_turn = 0
 
 var standard_deck : Array = [
 	["Clubs", "Ace", "A"], ["Hearts", "Ace", "A"], ["Spades", "Ace", "A"], ["Diamonds", "Ace", "A"], 
@@ -111,16 +113,16 @@ func set_round_order():
 			round_order_npcs.append(playing_npcs[0])
 			round_order_npcs.append(playing_npcs[2])
 		4:
-			round_order_npcs.append(playing_npcs[3])
 			round_order_npcs.append(playing_npcs[1])
 			round_order_npcs.append(playing_npcs[0])
 			round_order_npcs.append(playing_npcs[2])
+			round_order_npcs.append(playing_npcs[3])
 		5:
-			round_order_npcs.append(playing_npcs[3])
+			round_order_npcs.append(playing_npcs[4])
 			round_order_npcs.append(playing_npcs[1])
 			round_order_npcs.append(playing_npcs[0])
 			round_order_npcs.append(playing_npcs[2])
-			round_order_npcs.append(playing_npcs[4])
+			round_order_npcs.append(playing_npcs[3])
 
 func shuffle_deck():
 	#print("Shuffled Deck")
@@ -149,8 +151,6 @@ func square_deck():
 func distribute_cards():
 	##increases a number 
 	distribution_number += 1
-	##just to be sure i guess (i forgot why i had this)
-	gc_labels[6].text = "Dealer\nValue:" + str(player.dealer_hand_value) + "\nHand:\n"
 	
 	##defines how many cards need to be distributed
 	##two for every npc + two for dealer
@@ -172,10 +172,9 @@ func distribute_cards():
 		else:
 		##the npc's first and second cards are identical
 			#print("Distr " + str(distribution_number) + ", card 1 to npc " + str(int(floor(distribution_number / 3))))
-			##appends the top card to the npc's hand
-			round_order_npcs[int(floor(distribution_number / 3))].hand.append(deck.back())
-			##gives it to them
-			card_to_npc(deck.pop_back(), round_order_npcs[int(floor(distribution_number / 3))])
+			##gives a card to the npc
+			#round_order_npcs[int(floor(distribution_number / 3))].hand.append(deck.back())
+			card_to_npc(deck.pop_back(), round_order_npcs[int(floor(distribution_number / 3))], Vector3.ZERO)
 	else:
 		##the dealer's first card is side up
 		if distribution_number / 3 > round_order_npcs.size():
@@ -190,10 +189,9 @@ func distribute_cards():
 		else:
 		##the npc's first and second cards are identical
 			#print("Distr " + str(distribution_number) + ", card 2 to npc " + str(int(floor(distribution_number / 3))))
-			##appends the top card to the npc's hand
-			round_order_npcs[int(floor(distribution_number / 3))].hand.append(deck.back())
-			##gives it to them
-			card_to_npc(deck.pop_back(), round_order_npcs[int(floor(distribution_number / 3))])
+			##gives a card to the npc
+			#round_order_npcs[int(floor(distribution_number / 3))].hand.append(deck.back())
+			card_to_npc(deck.pop_back(), round_order_npcs[int(floor(distribution_number / 3))], Vector3.ZERO)
 
 ##tween to send a card to the dealer
 func card_to_dealer(card : Node3D, pos : Vector3, rot : Vector3):
@@ -208,14 +206,15 @@ func card_to_dealer(card : Node3D, pos : Vector3, rot : Vector3):
 	tween.tween_property(card, "rotation_degrees", rot, 0.66)
 
 ##tween to send a card to an npc
-func card_to_npc(card : Node3D, who : CharacterBody3D):
+func card_to_npc(card : Node3D, who : CharacterBody3D, rot : Vector3):
+	who.hand.append(card)
 	tween = create_tween()
 	tween.set_trans(Tween.TRANS_QUART)
 	tween.set_ease(Tween.EASE_OUT)
 	tween.set_parallel(true)
 	tween.tween_property(card, "global_position", who.hand_marker.global_position + Vector3(float(who.hand.size()) / 20,\
 	float(who.hand.size()) / 512, 0), 1)
-	tween.tween_property(card, "rotation_degrees", Vector3.ZERO, 0.66)
+	tween.tween_property(card, "rotation_degrees", rot, 0.66)
 	##updates the game control label
 	who.update_gc_label()
 
@@ -228,6 +227,7 @@ func pass_turn():
 	elif round_started == false and playing_npcs.size() <= 1:
 		pass
 	else:
+		turn_timer.start()
 		round += 1
 	gc_labels[5].text = "Turn " + str(round)
 
@@ -270,5 +270,25 @@ func _on_card_distr_timer_timeout() -> void:
 	if distribution_number < (round_order_npcs.size() + 1) * 3:
 		distribute_cards()
 	else:
-		print("Stopped timer")
+		#print("Stopped timer")
 		card_distr_timer.stop()
+
+func _on_turn_timer_timeout() -> void:
+	var npcs_playing = round_order_npcs.size()
+	
+	for npcs in round_order_npcs:
+		if npcs.state != "Playing":
+			npcs_playing -= 1
+	
+	if npcs_playing > 0:
+		if round_order_npcs[whose_turn].state == "Playing":
+			round_order_npcs[whose_turn].play_turn()
+		if whose_turn + 1 == round_order_npcs.size():
+			whose_turn = 0
+		else:
+			whose_turn += 1
+		turn_timer.start()
+		round += 1
+	else:
+		print("Dealer turn")
+		round += 1
