f1af92d412562ff5e0c05ad610e29b940abd75f7
preuss <87501978+felipepreuss@users.noreply.github.com>
2025-08-19 09:07:07 -0300

    spell sounds

---
+++ Scenes/spell_sounds.gd
@@ -0,0 +1,22 @@
+extends Node3D
+@onready var ad_maiorem_01: AudioStreamPlayer3D = $AdMaiorem01
+@onready var ad_maiorem_02: AudioStreamPlayer3D = $AdMaiorem02
+@onready var auri_sacra_01: AudioStreamPlayer3D = $AuriSacra01
+@onready var auri_sacra_02: AudioStreamPlayer3D = $AuriSacra02
+@onready var bellum_01: AudioStreamPlayer3D = $Bellum01
+@onready var bellum_02: AudioStreamPlayer3D = $Bellum02
+@onready var feitiço_sfx: AudioStreamPlayer3D = $FeitiçoSfx
+@onready var fortuna_01: AudioStreamPlayer3D = $Fortuna01
+@onready var fortuna_02: AudioStreamPlayer3D = $Fortuna02
+@onready var sapere_aude_01: AudioStreamPlayer3D = $SapereAude01
+@onready var sapere_aude_02: AudioStreamPlayer3D = $SapereAude02
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
