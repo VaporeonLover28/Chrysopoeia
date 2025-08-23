3c2b1cfb89bd1fb04f281a3567c150859b00608a
Solon <waltercarvalho2904@gmail.com>
2025-08-23 00:27:06 -0300

    2

--- options.gd
+++ options.gd
@@ -1,7 +1,11 @@
 extends Control
 
+func _process(delta: float) -> void:
+	if Input.is_action_just_pressed("esc"):
+		get_tree().change_scene_to_file("res://Scenes/main_menu.tscn")
+
 func _on_h_slider_value_changed(value: float) -> void:
-	$TabContainer/Audio/AudioStreamPlayer.play()
+	$TabContainer/Audio/galunga.play()
 	AudioServer.set_bus_volume_db(0,value)
 
 func _on_fullscreen_button_toggled(toggled_on: bool) -> void:
@@ -15,3 +19,15 @@ func _on_vsync_button_toggled(toggled_on: bool) -> void:
 		DisplayServer.window_set_vsync_mode(DisplayServer.VSYNC_DISABLED)
 	else:
 		DisplayServer.window_set_vsync_mode(DisplayServer.VSYNC_ENABLED)
+
+func _on_h_slider_2_value_changed(value: float) -> void:
+	AudioServer.set_bus_volume_db(1,value)
+	$TabContainer/Audio/galunga.play()
+
+func _on_h_slider_3_value_changed(value: float) -> void:
+	AudioServer.set_bus_volume_db(2,value)
+	$TabContainer/Audio/galunga2.play()
+
+func _on_h_slider_4_value_changed(value: float) -> void:
+	AudioServer.set_bus_volume_db(3,value)
+	$TabContainer/Audio/galunga3.play()
